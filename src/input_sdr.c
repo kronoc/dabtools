e3354923628ab632dc9b15091a870d4eb78e3a2c
Stefan Pöschel <github@basicmaster.de>
2016-11-01 10:51:04 +0100

    FIFO: read/write byte array instead of single bytes

--- src/input_sdr.c
+++ src/input_sdr.c
@@ -33,9 +33,7 @@ int sdr_demod(struct demapped_transmission_frame_t *tf, struct sdr_state_t *sdr)
   sdr->coarse_freq_shift = 0;
   
   /* write input data into fifo */
-  for (i=0;i<sdr->input_buffer_len;i++) {
-    cbWrite(&(sdr->fifo),&sdr->input_buffer[i]);
-  }
+  cbWriteBytes(&sdr->fifo, sdr->input_buffer, sdr->input_buffer_len);
 
   /* Check for data in fifo */
   if (sdr->fifo.count < 196608*3) {
